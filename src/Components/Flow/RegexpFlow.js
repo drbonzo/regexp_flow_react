b723b13b6360dbc78adea288a02583d20d1f2251
dr_bonzo <dr_bonzo@northslope.pl>
2016-10-12 22:08:39 +0200

    Added RegexpFlowMetadata description and onDescriptionChange component properties

--- src/Components/Flow/RegexpFlow.js
+++ src/Components/Flow/RegexpFlow.js
@@ -9,10 +9,28 @@ import MatchInLineTextProcessor from '../Flow/MatchInLineTextProcessor.js';
 import UniqueTextProcessor from '../Flow/UniqueTextProcessor.js';
 
 class RegexpFlow extends Component {
+
+	constructor() {
+		super();
+
+		// FIXME application state!
+		this.state = {
+			description: "Sample workflow description"
+		};
+
+		this.onFlowDescriptionChanged = this.onFlowDescriptionChanged.bind(this);
+	}
+
+	onFlowDescriptionChanged(event) {
+		this.setState({
+			description: event.target.value
+		});
+	}
+
 	render() {
 		return (
 			<div className="RegexpFlow">
-				<RegexpFlowMetadata/>
+				<RegexpFlowMetadata description={this.state.description} onDescriptionChange={this.onFlowDescriptionChanged}/>
 				<div className="RegexpFlow__TextProcessors">
 					<MatchLinesTextProcessor searchRegexp="aaaa" caseInsensitive={true} invertMatch={true} description="some matching lines"/>
 					<FindAllTextProcessor searchRegexp="bbb" caseInsensitive={true} description="some find all"/>
