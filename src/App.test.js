cba77e4bbd3035fe63e9b2ad1abead9ae50e31e1
dr_bonzo <dr_bonzo@northslope.pl>
2016-10-16 12:43:56 +0200

    Fixed App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import App from './App';
 
 it('renders without crashing', () => {
+
+	let store = createStore(function (state, action) {
+		return state
+	}, {description: '', textProcessors: {}});
+
 	const div = document.createElement('div');
-	ReactDOM.render(<App />, div);
+	ReactDOM.render(
+		<Provider store={store}>
+			<App />
+		</Provider>
+		, div);
 });
