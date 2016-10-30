a096cbe670f900d55f09dc4c458f1f8bbb140a22
dr_bonzo <dr_bonzo@northslope.pl>
2016-10-30 11:10:16 +0100

    mainReducer() computes state.outputText

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,16 +1,22 @@
 import description from './description'
 import filterConfigs from './filterConfigs'
 import inputText from './inputText'
+import FilterRunner from '../../RegexpFlow/FilterRunner'
 
 const mainReducer = function (state, action) {
 	let filterConfigsNew = filterConfigs(state.filterConfigs, action);
 	let descriptionNew = description(state.description, action);
 	let inputTextNew = inputText(state.inputText, action);
 
+	let runner = new FilterRunner();
+	// FIXME update totals/counters in filter configs
+	let outputTextNew = runner.processString(filterConfigsNew, inputTextNew);
+
 	return Object.assign({}, state, {
 		filterConfigs: filterConfigsNew,
 		description: descriptionNew,
-		inputText: inputTextNew
+		inputText: inputTextNew,
+		outputText: outputTextNew
 	});
 };
 
