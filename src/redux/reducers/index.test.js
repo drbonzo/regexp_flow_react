0d2015ded461578b17efd67c579f13b4957ba2c1
dr_bonzo <dr_bonzo@northslope.pl>
2016-10-30 10:30:28 +0100

    Added reduceres/index.test.js

---
+++ src/redux/reducers/index.test.js
@@ -0,0 +1,48 @@
+import mainReducer from './index'
+
+import {
+	updateInputText,
+	regxpFlowUpdateDescription,
+	regexpFlowAddFilter
+} from '../actions'
+
+import FindAllFilterConfig from '../../RegexpFlow/FilterConfig/FindAllFilterConfig'
+
+describe("mainReducer", function () {
+
+	var state;
+
+	beforeEach(function () {
+		state = {
+			filterConfigs: {},
+			description: '',
+			inputText: '',
+		};
+	});
+
+	it('should handle inputText', function () {
+		let expectedState = Object.assign({}, state);
+		expectedState.inputText = 'new inputText';
+
+		let newState = mainReducer(state, updateInputText('new inputText'));
+		expect(newState).toEqual(expectedState);
+	});
+
+	it('should handle description', function () {
+		let expectedState = Object.assign({}, state);
+		expectedState.description = 'new description';
+
+		let newState = mainReducer(state, regxpFlowUpdateDescription('new description'));
+		expect(newState).toEqual(expectedState);
+	});
+
+	it('should handle filterConfigs', function () {
+		let expectedState = Object.assign({}, state);
+		expectedState.filterConfigs[1] = new FindAllFilterConfig();
+		expectedState.filterConfigs[1].id = 1;
+		expectedState.filterConfigs[1].searchString = "\\b.+?\\b";
+
+		let newState = mainReducer(state, regexpFlowAddFilter('FindAll'));
+		expect(newState).toEqual(expectedState);
+	});
+});
