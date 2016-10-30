e6e26d10835bfa0accbef8f43748a784ef660a06
dr_bonzo <dr_bonzo@northslope.pl>
2016-10-30 11:01:16 +0100

    Refactoring reducers: removed usage of combineReducers()

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,17 @@
 import description from './description'
 import filterConfigs from './filterConfigs'
 import inputText from './inputText'
-import {combineReducers} from 'redux'
 
-const mainReducer = combineReducers({
-	filterConfigs: filterConfigs,
-	description: description,
-	inputText: inputText,
-});
+const mainReducer = function (state, action) {
+	let filterConfigsNew = filterConfigs(state.filterConfigs, action);
+	let descriptionNew = description(state.description, action);
+	let inputTextNew = inputText(state.inputText, action);
+
+	return Object.assign({}, state, {
+		filterConfigs: filterConfigsNew,
+		description: descriptionNew,
+		inputText: inputTextNew
+	});
+};
 
 export default mainReducer;
